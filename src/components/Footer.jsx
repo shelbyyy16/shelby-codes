819d4c4934a5a0d8bf8d72571e6b51cb5aaaa826
honeybee <shelby.e.pagan@gmail.com>
2024-03-21 14:16:33 -0400

    add navbar and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,37 @@
+import { Link } from "react-router-dom";
+
+
 function Footer() {
-    return <h1>Footer</h1>;
-  }
-  
-  export default Footer;
+  return (
+    <footer className="page-footer">
+      <div className="container">
+        <div className="row">
+          <div className="col l6 s12">
+            <h5 className="white-text">Shelby Codes</h5>
+            <p className="grey-text text-lighten-4">
+              Full Stack Developer
+            </p>
+          </div>
+          <div className="col l4 offset-l2 s12">
+            <h5 className="white-text">Explore More</h5>
+            <ul>
+            <li><Link to="/about"><div>ABOUT</div></Link></li>
+            <li><Link to="/projects"><div>PROJECTS</div></Link></li>
+            <li><Link to="/contact"><div>CONTACT</div></Link></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="footer-copyright">
+        <div className="container">
+          © 2024 Shelby Pagan
+          <a className="grey-text text-lighten-4 right" href="#!">
+            More Links
+          </a>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Header() {
-    return <h1>Header</h1>;
-  }
-  
-  export default Header;
-  
+
+  return (
+    <header>
+  <nav>
+    <div className="nav-wrapper">
+    <Link to="/" className="brand-logo"><div>LOGO</div></Link>
+      <ul id="nav-mobile" className="right hide-on-med-and-down">
+      <li><Link to="/about"><div>ABOUT</div></Link></li>
+      <li><Link to="/projects"><div>PROJECTS</div></Link></li>
+      <li><Link to="/contact"><div>CONTACT</div></Link></li>
+      </ul>
+    </div>
+  </nav>
+    </header>
+  );
+}
+
+export default Header;

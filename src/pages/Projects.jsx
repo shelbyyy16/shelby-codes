c2d619606af39f41a23e0324f6e730e542b0097a
honeybee <shelby.e.pagan@gmail.com>
2024-04-10 14:08:40 -0400

    add hover effects and project info to projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,41 +1,53 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import projectsData from "/public/projects.json"; 
 
 function Projects() {
-    return (
+  const [hoveredProject, setHoveredProject] = useState(null); // State to track hovered project
+
+  return (
     <>
-  <div id="projects-header">
-    <h3>It All Begins With An Idea</h3>
-    <br></br><br></br>
-      <h5>Here are the results of dozens of cups of coffee, many lines of code and hours of debugging</h5>
-  </div>
-  <div id="landing-portfolio" className="row">
-        <div className="col s4">
-          <Link to='/projects/cartquest'><img
-            src="/images/CartQuestMockUp.png"
-            id="landing-portfolio-item"
-          ></img></Link>
-        </div>
-        <div className="col s4">
-          <Link to='/projects/petalsandpots'><img
-            src="/images/PetalsandPots MockUp.png"
-            id="landing-portfolio-item"
-          ></img></Link>
-        </div>
-        <div className="col s4">
-          <Link to='/projects/split'><img src="/images/SPLITMockUp.png" id="landing-portfolio-item"></img></Link>
-        </div>
+      <div id="projects-header">
+        <h3>It All Begins With An Idea</h3>
+        <br></br>
+        <br></br>
+        <h5>
+          Here are the results of dozens of cups of coffee, many lines of code,
+          and hours of debugging
+        </h5>
+      </div>
+      <div id="landing-portfolio" className="row">
+        {projectsData.map((project, index) => (
+          <div className="col s4" key={index}>
+            <Link to={`/projects/${project.name}`}>
+              <img
+                src={project.image}
+                id="landing-portfolio-item"
+                alt={project.name}
+                onMouseEnter={() => setHoveredProject(project)}
+                onMouseLeave={() => setHoveredProject(null)}
+                className="project-image" 
+              />
+            </Link>
+            {/* Display project info if hovered */}
+            {hoveredProject === project && (
+              <div className="project-info">
+                <h4>{project.name}</h4>
+                <p>{project.tagline}</p>
+                <p>{project.description}</p>
+                {/* Add buttons for live and GitHub links if needed */}
+                {/* <a href={project.live}>Live</a>
+                <a href={project.git}>GitHub</a> */}
+              </div>
+            )}
+          </div>
+        ))}
         <div
           className="col s12"
           id="landing-portfolio-button"
           style={{ display: "flex", justifyContent: "center" }}
-        >
-        </div>
+        ></div>
       </div>
-
-
     </>
-
-    )
-  }
-  
-  export default Projects;
+  );
+}

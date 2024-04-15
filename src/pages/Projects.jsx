6558ef950f4af3660fa8ccdc646b28aab5afd5f3
honeybee <shelby.e.pagan@gmail.com>
2024-04-15 16:31:23 -0400

    take out hover effect. display project info

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,8 @@
-import React, { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 import projectsData from "/src/projects.json"; 
 
 function Projects() {
-  const [hoveredProject, setHoveredProject] = useState(null); // State to track hovered project
-
   return (
     <>
       <div id="projects-header">
@@ -18,39 +16,31 @@ function Projects() {
       </div>
       <div id="landing-portfolio" className="row">
         {projectsData.map((project, index) => (
-          <div className="col s4" key={index}>      
+          <div className="col s4" key={index}>
+            <div>
               <img
                 src={project.image}
-                id="landing-portfolio-item"
                 alt={project.name}
-                onMouseEnter={() => setHoveredProject(project)}
-                onMouseLeave={() => setHoveredProject(null)}
-                className="project-image" 
+                id="landing-portfolio-item"
               />
-            {/* Display project info if hovered */}
-            {hoveredProject === project && (
               <div className="project-info">
                 <h4>{project.name}</h4>
                 <p>{project.tagline}</p>
                 <p>{project.description}</p>
-                <p><Link to={project.live}>
+                <div>
+                  <Link to={project.live}>
                     <button id="secondary-button">View Live</button>
-                    </Link>
-                    <Link to={project.git}>
+                  </Link>
+                  <Link to={project.git}>
                     <button id="secondary-button">GitHub</button>
-                    </Link> </p>
+                  </Link>
+                </div>
               </div>
-            )}
+            </div>
           </div>
         ))}
-        <div
-          className="col s12"
-          id="landing-portfolio-button"
-          style={{ display: "flex", justifyContent: "center" }}
-        ></div>
       </div>
     </>
   );
 }
 export default Projects;
-
